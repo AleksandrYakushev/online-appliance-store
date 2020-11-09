664c9506181e6de4a3f1f09a79a02f5b9ca08037
Alesksandr <haman282828@yandex.ru>
2020-11-09 09:49:23 +0300

    Fixed dapper Merge Customer

--- OnlineApplianceStore.Business/Hashing/BCryptingHashing.cs
+++ OnlineApplianceStore.Business/Hashing/BCryptingHashing.cs
@@ -1,10 +1,32 @@
-﻿using System;
+﻿using BCrypt.Net;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace OnlineApplianceStore.Business
+
+namespace OnlineApplianceStore.Business.Hashing
 {
-    public class BCryptingHashing
+    public class BCryptHashing
     {
+
+        public static string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password, Salt.Getsalt());
+        }
+
+        public static bool ValidatePassword(string password, string correctHash)
+        {
+
+
+            for (int i = 0; i < Salt.listSalt.Count; i++)
+            {
+                var hashPass = BCrypt.Net.BCrypt.HashPassword(password, Salt.listSalt[i]);
+                if (hashPass == correctHash)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 }
